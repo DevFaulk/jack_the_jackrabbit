15e9e60920aaa28775f846ad66b1420d3cb71065
devfaulk <faulkdev@gmail.com>
2024-10-01 01:52:17 -0400

    add movement

--- main.lua
+++ main.lua
@@ -1,36 +1,50 @@
+require("snake")
+
 -- Handle game quit
 function love.keypressed(key)
   if key == "escape" then
     love.event.quit()
   end
-end
+  -- Handle snake movement
+  if key == "right" or key == "left" or key == "down" or key == "up" then
+    for index, segment in ipairs(Snake.segments) do
+      if key == "right" then
+        segment.x = segment.x + 1
+      end
+      if key == "left" then
+        segment.x = segment.x - 1
+      end
+      if key == "down" then
+        segment.y = segment.y - 1
+      end
+      if key == "up" then
+        segment.y = segment.y + 1
+      end
+    end
+  end
 
-function love.draw()
+
+  function love.draw()
     local gridXCount = 20
     local gridYCount = 15
     local cellSize = 20
 
-    snakeSegments = {
-        { x = 3, y = 1 },
-        { x = 2, y = 1 },
-        { x = 1, y = 1 }
-    }
-
     scoreSoFar = 0
 
-    for segmentIndex, segment in ipairs(snakeSegments) do
-        score(scoreSoFar, segment, cellSize)
+    for segmentIndex, segment in ipairs(Snake.segments) do
+      score(scoreSoFar, segment, cellSize)
     end
-end
+  end
 
-function score(scoreSoFar, segment, cellSize)
+  function score(scoreSoFar, segment, cellSize)
     scoreSoFar = scoreSoFar + 1 --increment score so far
     love.graphics.setColor(.9, .9, .9)
     love.graphics.rectangle(
-        'fill',
-        (segment.x - 1) * cellSize,
-        (segment.y - 1) * cellSize,
-        cellSize - 1,
-        cellSize - 1
+      'fill',
+      (segment.x - 1) * cellSize,
+      (segment.y - 1) * cellSize,
+      cellSize - 1,
+      cellSize - 1
     )
+  end
 end
